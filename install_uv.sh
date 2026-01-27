9322cfe167142f94d7abca1a2189a2c009da555b
Zhijie Yan <yanzhijie@buaa.edu.cn>
2026-01-27 16:36:35 +0800

    add install_uv.sh

---
+++ install_uv.sh
@@ -0,0 +1,20 @@
+# 1. (Optional) install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv --version
+
+# 2. (Optional) install cuda-12.1
+wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda_12.8.0_570.86.10_linux.run
+sudo sh cuda_12.8.0_570.86.10_linux.run
+rm -rf cuda_12.8.0_570.86.10_linux.run
+## set ~/.bashrc to overwrite cuda-12.8
+
+# 3. create python 3.10 environment
+# uv venv --python 3.10
+uv venv .venv_libero --python 3.10 --prompt cosmos_policy_libero
+
+# 4. activate environment and install environments
+source .venv_libero/bin/activate
+uv sync --active --extra cu128 --group libero
+
+# deactivate environment
+deactivate
